4b7848f5b542e9adbad921af8895a26c8206e71a
rajnishQuadb <rajnish.tripathi@quadbtech.com>
2024-09-25 18:04:10 +0530

    UI: about section created and Title component created

--- src/Tap-Tap_Go-frontend/src/pages/HomePage.jsx
+++ src/Tap-Tap_Go-frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../components/common/Footer/Footer'
 import LowerSection from '../components/home/LowerSection/LowerSection'
+import About from '../components/home/About/About'
 
 export default function HomePage() {
   return (
     <div className='home'>
       HomePage works!
+      <About />
       <LowerSection />
       <Footer />
     </div>
