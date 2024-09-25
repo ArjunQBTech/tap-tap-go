3ce154745a1380276853286c99b3b94498ef4607
atharav-bhatnagar-qb <atharav.bhatnagar@quadbtech.com>
2024-09-25 13:30:17 +0530

    tailwind setup added

--- src/Tap-Tap_Go-frontend/src/App.jsx
+++ src/Tap-Tap_Go-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Routes,Route } from 'react-router-dom';
-import HomePage from './components/page/HomePage';
+import HomePage from './pages/HomePage';
 
 function App() {
 
