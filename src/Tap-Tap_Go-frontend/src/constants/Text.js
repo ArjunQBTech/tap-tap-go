608ff5684c3b1943e26517efb0a9f35cafd7f755
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-09-25 17:13:29 +0530

    ui 🔧 : Added Hero component styles and Updated text content

--- src/Tap-Tap_Go-frontend/src/constants/Text.js
+++ src/Tap-Tap_Go-frontend/src/constants/Text.js
@@ -1,1 +1,10 @@
-export const Text={}
+export const Text={
+    'home' : {
+        'hero' : {
+            'title' : "EXPLORE A WORLD OF CUTE MYTHICAL CREATURES",
+            'img' : "/assets/images/hero.jpeg",
+            'subtitle' : 'Embark on a journey to discover the cutest mythical creatures from around the world',
+            'cta' : 'Get Early Access'
+        }
+    }
+}
