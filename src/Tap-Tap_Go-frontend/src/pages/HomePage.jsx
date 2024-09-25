df3455cdd0c3f129650cf58399331012f53be337
atharav-bhatnagar-qb <atharav.bhatnagar@quadbtech.com>
2024-09-25 17:38:48 +0530

    imported hero section

--- src/Tap-Tap_Go-frontend/src/pages/HomePage.jsx
+++ src/Tap-Tap_Go-frontend/src/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Rules from '../components/home/rules-sec/Rules'
 import Footer from '../components/common/Footer/Footer'
 import LowerSection from '../components/home/LowerSection/LowerSection'
+import Hero from '../components/home/Hero'
 
 export default function HomePage() {
   return (
     <div className='page'>
+      <Hero/>
       <Rules/>
       <LowerSection />
       <Footer />
